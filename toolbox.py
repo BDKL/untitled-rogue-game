56e2675cdfa4c0a982f25f588e9903064b6fe5f3
Nathan <121110414@umail.ucc.ie>
2023-12-05 23:43:28 +0000

    new script

--- toolbox.py
+++ toolbox.py
@@ -3,15 +3,32 @@ import sys
 import subprocess
 import os
 
+def update_repo(repo_path):
+    try:
+        # Open the repository
+        repo = git.Repo(repo_path)
+
+        # Fetch changes from the remote repository
+        origin = repo.remote(name='untitled-rogue-game')
+        origin.fetch()
+
+        # Merge changes
+        repo.git.merge('origin/main') 
+
+        print("Repository updated successfully.")
+
+    except git.exc.GitCommandError as e:
+        print(f"Error occurred: {e}")
+
 def compile_and_push_or_commit(repo_path, commit_message):
     try:
-        # Check if the argument is 'compile'
+        
         if commit_message.lower() == 'compile':
             # Run scons command in the current directory
             subprocess.run(['scons', 'platform=windows'])
             print("Compilation completed.")
         else:
-            # Open the repository
+            update_repo(repo_path)
             repo = git.Repo(repo_path)
 
             # Add all files
@@ -20,7 +37,7 @@ def compile_and_push_or_commit(repo_path, commit_message):
             # Commit changes
             repo.index.commit(commit_message)
 
-            # Push changes to the remote repository
+            # Push changes to the repo
             origin = repo.remote(name='untitled-rogue-game')
             origin.push()
 
@@ -29,20 +46,20 @@ def compile_and_push_or_commit(repo_path, commit_message):
     except git.exc.GitCommandError as e:
         print(f"Error occurred: {e}")
 
-# Provide the path to your local repository
 repository_path = os.getcwd()
 
-# Get the commit message from command line arguments or use a default message
 if len(sys.argv) > 1:
     commit_msg = ' '.join(sys.argv[1:])
 else:
     commit_msg = 'Default commit message'
 
-# Check if the argument is 'compile' or perform commit and push
+
 if commit_msg.lower() == 'compile':
     # Run scons command in the current directory
     current_directory = os.getcwd()
     subprocess.run(['scons', 'platform=windows'], cwd=current_directory)
     print("Compilation completed.")
+elif commit_msg.lower() == 'update':
+    update_repo(repository_path)
 else:
     compile_and_push_or_commit(repository_path, commit_msg)
