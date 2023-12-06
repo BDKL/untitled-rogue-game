9afb143cc65affddaab32afe1e8c59c6095eafb6
NathanielDev <73847900+NathanielDev@users.noreply.github.com>
2023-12-06 00:20:15 +0000

    Update toolbox.py

--- toolbox.py
+++ toolbox.py
@@ -13,7 +13,7 @@ def update_repo(repo_path):
         origin.fetch()
 
         # Merge changes
-        repo.git.merge('untitled-rogue-game/master') 
+        repo.git.merge('origin/master') 
 
         print("Repository updated successfully.")
 
