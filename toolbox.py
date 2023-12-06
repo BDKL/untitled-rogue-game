6d58ff4c04270521c8c830ed845570ab6683fa32
PiresDevBDKL <Meuemaildecriarcontas@gmail.com>
2023-12-05 21:35:49 -0300

    Default commit message

--- toolbox.py
+++ toolbox.py
@@ -13,7 +13,7 @@ def update_repo(repo_path):
         origin.fetch()
 
         # Merge changes
-        repo.git.merge('origin/master') 
+        repo.git.merge('origin/piroca') 
 
         print("Repository updated successfully.")
 
