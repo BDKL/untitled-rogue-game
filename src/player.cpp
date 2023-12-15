2bcb854f1f43a2c21a46b8f74a384d4c6298621a
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2023-12-15 17:14:03 -0300

    player.cpp currently recognizing movement keys being pressed

--- src/player.cpp
+++ src/player.cpp
@@ -4,17 +4,59 @@
 #include <godot_cpp/core/class_db.hpp>
 
  Player::Player() {
+//    I don't know what to add here
+     UtilityFunctions::print("player started");
 
 }
 
 Player::~Player(){
-
+// clean up
 }
 
-void Player::start_player() {
-    UtilityFunctions::print("i love gdextension");
+void Player::move_up() {
+    x_coord+=1;
+    UtilityFunctions::print("up");
+}
+
+void Player::move_left() {
+    x_coord-=1;
+    UtilityFunctions::print("left");
+}
+
+void Player::move_down() {
+    x_coord-=1;
+    UtilityFunctions::print("down");
+}
+
+void Player::move_right() {
+    y_coord+=1;
+    UtilityFunctions::print("right");
+}
+
+void Player::check_movement() {
+    Input *input = Input::get_singleton();
+
+    if (input->is_key_pressed(KEY_W)) {
+        move_up();
+    }
+
+    if (input->is_key_pressed(KEY_A)) {
+        move_left();
+    }
+
+    if (input->is_key_pressed(KEY_S)) {
+        move_down();
+    }
+
+    if (input->is_key_pressed(KEY_D)) {
+        move_right();
+    }
 }
 
  void Player::_bind_methods() {
-   ClassDB::bind_method(D_METHOD("start_player"), &Player::start_player);
+     ClassDB::bind_method(D_METHOD("check_movement"), &Player::check_movement);
+     ClassDB::bind_method(D_METHOD("move_up"), &Player::move_up);
+     ClassDB::bind_method(D_METHOD("move_left"), &Player::move_left);
+     ClassDB::bind_method(D_METHOD("move_down"), &Player::move_down);
+     ClassDB::bind_method(D_METHOD("move_right"), &Player::move_right);
  }
--- src/player.h
+++ src/player.h
@@ -2,15 +2,19 @@
 #define PLAYER_H
 
 #include <godot_cpp/classes/area2d.hpp>
-// #include <godot_cpp/classes/animated_sprite2d.hpp>
-// #include <godot_cpp/classes/collision_object2d.hpp>
-// #include <godot_cpp/godot.hpp>
-// #include <godot_cpp/classes/input.hpp>
+#include <godot_cpp/classes/animated_sprite2d.hpp>
+#include <godot_cpp/classes/collision_object2d.hpp>
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/input.hpp>
 
 using namespace godot;
 
 class Player : public Area2D {
   GDCLASS(Player, Area2D);
+
+private:
+    float x_coord=0;
+    float y_coord=0;
 
 protected:
     static void _bind_methods();
@@ -19,7 +23,12 @@ public:
     Player();
     ~Player();
 
-    void start_player();
+    void check_movement();
+    void move_up();
+    void move_left();
+    void move_down();
+    void move_right();
+
 };
 
-#endif
+#endif // PLAYER_H
